5173857c8b21d1ba93f7f00dad15f977c64dbd02
scottwatanuki <scottweagle2@gmail.com>
2022-06-29 23:55:27 -1000

    Update test.py with content on functions

--- test.py
+++ test.py
@@ -1,28 +1,30 @@
-#This is a comment in python!
+#Welcome to Programming!
+    #This is a comment in python!
 
-#type function 
+#Values, Variables, and Data Types
+    #type function 
 print(type("Hello World"))
 print(type(15))
 print(type(True))
 print(type(78.8))
 print(type("78.8"))
 
-#type conversion
-#1. int
+    #type conversion
+        #1. int
 print(int(-3.4))
 print(int(2.999))
 print(int("2345"))
 #print(int("23 bottles")) 
     #invalid
 
-#2. float
+        #2. float
 print(float(17))
 print(float("123.45"))
 
-#3. string
+        #3. string
 print(str(17))
 
-#Mathematical Operation - Priority
+    #Mathematical Operation - Priority
 print(84 + 10)
 print(84 - 10)
 print(84 * 10)
@@ -34,9 +36,20 @@ print(84 % 10)
 #if any of the numbers is a float, the result is a float.
 #HOWEVER, integer division with a float with yield an integer answer as a floating point rather than the correct decimal approximation. (i.e., 11//3.0 = 3.0, not 3)
 
-#Operations on Strings
+    #Operations on Strings
 print("Hello" + " World")
 print("Hello" * 3)
 print(len("Hello World"))
-n = input("Enter your name: ")
+#n = input("Enter your name: ")
 print("Hello " + n  + "!")
+
+    #Useful functions
+print(len("Happy Birthday"))
+#n = input("Enter your name: ")
+
+#Functions
+def addTwoNums (x,y) : 
+    sumOfNum = x + y
+    print("The sum of the two given numbers is " + sumOfNum + ".")
+
+#Print vs Return
